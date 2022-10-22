809e2604ce3b29737b84d1b9ba55ef3feec8ad3a
Sachin.Sharma <sachin.sharma@target.com>
2022-10-23 01:37:09 +0530

    added class design unit

--- Class_Design/Q6_get_random.py
+++ Class_Design/Q6_get_random.py
@@ -3,23 +3,39 @@ import random
 class RandomizedSet:
 
     def __init__(self) -> None:
-        self.random_set = set()
         self.random_list = []
+        self.value_to_idx = {}
 
     def insert(self, val):
-        if val in self.random_set:
+        if val in self.value_to_idx:
             return False
 
-        self.random_set.add(val)
-        self.random_list.append(val)
+        self.random_list.add(val)
+        last_idx = len(self.random_list) - 1
+        self.value_to_idx[val] = last_idx
         return True
 
-    def remove(self, val):
-        if val not in self.random_set:
-            return False
+    def swapValues(self, i, j):
+        i_val = self.random_list[i]
+        j_val = self.random_list[j]
 
-        self.random_set.remove(val)
-        self.random_list.remove(val)
+        self.random_list[i], self.random_list[j] = j_val, i_val
+        self.value_to_idx[i_val] = j
+        self.value_to_idx[j_val] = i
+
+
+    def remove(self, val):
+        if val not in self.value_to_idx:
+            return False
+        
+        last_idx = len(self.random_list) - 1
+        remove_idx = self.value_to_idx[val]
+        self.swapValues(remove_idx, last_idx)
+
+        del self.value_to_idx[val]
+
+        self.random_list.pop()
+        
         return True
 
     def getRandom(self):
